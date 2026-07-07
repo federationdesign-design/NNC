483e22de5bb79e9c91445f4dc6ea864f788ecc31
Steve James <federation.design@gmail.com>
2026-07-07 14:32:45 +0100

    Add vacancies and homes page routes

---
+++ app/homes/page.tsx
@@ -0,0 +1,10 @@
+import OurHomesPage from "../../components/home/OurHomesPage";
+
+export const metadata = {
+  title: "Our Homes | Nurturing Nests Care",
+  description: "Specialist residential children's homes in Kent. Ofsted registered and inspected.",
+};
+
+export default function Page() {
+  return <OurHomesPage />;
+}
---
+++ app/vacancies/page.tsx
@@ -0,0 +1,10 @@
+import VacanciesPage from "../../components/home/VacanciesPage";
+
+export const metadata = {
+  title: "Vacancies | Nurturing Nests Care",
+  description: "Current job vacancies at Nurturing Nests Care. Join our values-led team in Kent.",
+};
+
+export default function Page() {
+  return <VacanciesPage />;
+}
