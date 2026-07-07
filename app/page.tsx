91028725428bdb2c26827e98b7a400ea14c83540
Steve James <federation.design@gmail.com>
2026-07-07 15:19:17 +0100

    Swap homepage to v2, fix QuoteSection button styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 import HomeHeroV2 from "./components/home/HomeHeroV2";
-import ApproachStrip from "./components/home/ApproachStrip";
-import TeamBand from "./components/home/TeamBand";
-import JobsBand from "./components/home/JobsBand";
+import UtilityBar2 from "./components/home/UtilityBar2";
+import TeamBand2 from "./components/home/TeamBand2";
 import QuoteSection from "./components/home/QuoteSection";
 
 export default function HomePage() {
   return (
-    <>
+    <div style={{ position: "relative" }}>
       <HomeHeroV2 />
-      <ApproachStrip />
-      <TeamBand />
-      <JobsBand />
+      <UtilityBar2 />
+      <TeamBand2 />
       <QuoteSection />
-    </>
+    </div>
   );
 }
