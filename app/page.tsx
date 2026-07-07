90268e3e035bb693837c697fa5f1b4cc925a2128
Steve James <federation.design@gmail.com>
2026-07-07 17:16:18 +0100

    Remove position:relative wrapper, add homepage metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,18 @@ import UtilityBar2 from "./components/home/UtilityBar2";
 import TeamBand2 from "./components/home/TeamBand2";
 import QuoteSection from "./components/home/QuoteSection";
 
+export const metadata = {
+  title: "Nurturing Nests Care | Specialist Residential Care in Kent",
+  description: "Small, structured children's homes in Kent providing therapeutic residential care for children with emotional, behavioural and relational needs. Ofsted Good Provider.",
+};
+
 export default function HomePage() {
   return (
-    <div style={{ position: "relative" }}>
+    <>
       <HomeHeroV2 />
       <UtilityBar2 />
       <TeamBand2 />
       <QuoteSection />
-    </div>
+    </>
   );
 }
